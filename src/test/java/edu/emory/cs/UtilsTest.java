40c08884f0a507ca32a639a1579607472a4ccc80
Your Name <your_id@emory.edu>
2023-02-11 22:45:38 -0500

    check

---
+++ src/test/java/edu/emory/cs/UtilsTest.java
@@ -0,0 +1,11 @@
+package edu.emory.cs;
+// check
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UtilsTest {
+    @Test
+    public void getMiddleIndexTest() {
+        assertEquals(5, Utils.getMiddleIndex(0, 10));
+    }
+}
